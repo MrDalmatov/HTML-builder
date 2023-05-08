37e8d012084cb554a43df1431e33a723938b7d4d
Rodion <mrDalmatov@MacBook-Pro-Rodion.local>
2023-05-08 21:30:13 +0700

    feat: task 04 complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+async function copyDir(source, target) {
+  try {
+    await fs.promises.mkdir(target, { recursive: true });
+    const files = await fs.promises.readdir(source, { withFileTypes: true });
+    for (const file of files) {
+      const sourcePath = path.join(source, file.name);
+      const targetPath = path.join(target, file.name);
+      if (file.isFile()) {
+        await fs.promises.copyFile(sourcePath, targetPath);
+      } else if (file.isDirectory()) {
+        await copyDir(sourcePath, targetPath);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir(sourceDir, targetDir);
